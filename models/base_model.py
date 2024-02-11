e6a9a4bc0cb362906b6480bf73cfe2b12b2d052a
Marvel paul <djmarvel@yahoo.com>
2024-02-10 17:20:21 -0800

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    This module defines theBaseModel class
+    This module defines the BaseModel class
 """
 
 import uuid
@@ -8,34 +8,42 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        self.time_fmt = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, self.time_fmt))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """
-        Updates the public instance attribute:
-        'updated_at' - with the current datetime
+        Updates the public instance attribute 'updated_at'
+        with the current datetime
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Method returns a dictionary containing all
-        keys/values of __dict__ instance of BaseModel class
+        Method returns a dictionary containing all keys/values of
+        ___dict__ instance of BaseModel class
         """
-        inst_dict = dict(self.__dict__)
+        inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
         inst_dict["created_at"] = self.created_at.isoformat()
         inst_dict["updated_at"] = self.updated_at.isoformat()
-
         return inst_dict
 
     def __str__(self):
         """
-         Return string representation of BaseModel class
+        Return string representation of BaseModel class
         """
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
-
