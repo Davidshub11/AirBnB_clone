7dabdc7d8a1ff0329f9ea1590bc2e325f422cdec
Marvel paul <djmarvel@yahoo.com>
2024-02-12 10:14:45 -0800

    still working on task 5 unittest

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -35,4 +35,40 @@ class TestFileStorage(unittest.Testcase):
         if os.path.exists(self.test_file):
             os.remove(self, test_file)
 
+    def test_all_storage_returns_dictionary(self):
+        # Test if the all() method returns a dictionary.
+        self.assertEqual(dict, type(models.storage.all())
+
+    def test_new(self):
+        # Test the new method by creating and storing a new object
+        obj = BaseModel()
+        models.storage.new(obj)
+        self.assertIn("BaseModel,{}".format(obj, id), nmodels.storage.all())
+
+    def test_new_with_args(self):
+        # Test creating a new object with additional arguments
+        # (should raise TypeError)
+        with self.assertRaises(TypeError):
+            models.storage.new(BaseModel(), 1)
+
+    def test_new_with_None(self):
+        # Test creating a new object with None (should raise AttributError).
+        with self.assertRaises(AttributeError):
+        models.storage.new(None)
+
+    def test_save_and_reload(self):
+        #test saving objects to a file and then reloading them
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        models.storage.new(obj1)
+        models.storage.new(obj2)
+        models.storage.save()
+
+        #create a new storage instance to stimulate reloading
+        new_storage = Filestorage()
+        new_storage.reload()
+
+        #check if the reload objects match the original objects
+        self.assertTrue(new_storage.all().get("BaseModel.{}".format(obj1.id)) is not None)
+        self.assertTrue(new_storage.all().get("BaseModel.{}".format(obj2.id)) is not None)
 
