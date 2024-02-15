f3b2e824d88b06c9fc6ae9dd5890b349b14cbcc7
Marvel paul <djmarvel@yahoo.com>
2024-02-15 04:39:08 -0800

    fixed bugs for test_base_model

--- models/base_model.py
+++ models/base_model.py
@@ -57,7 +57,7 @@ class BaseModel:
         Updates the public instance attribute:
         'updated_at' - with the current datetime
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
